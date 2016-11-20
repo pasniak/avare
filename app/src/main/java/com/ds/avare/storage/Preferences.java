24abf83638aae75dffc2c17f58a1746361727f75
pasniak <michalp@yahoo.com>
2016-11-20 08:03:18 -0500

    Shorten the list of Winds Aloft by displaying winds up to the level set in Preferences (https://i.imgur.com/E1E64uY.png)

--- app/src/main/java/com/ds/avare/storage/Preferences.java
+++ app/src/main/java/com/ds/avare/storage/Preferences.java
@@ -1212,6 +1212,13 @@ public class Preferences {
         mPref.edit().putInt("rateAskLastCount", set).commit();
     }
 
+    public int getWindsAloftCeiling() {
+        try {
+           return Integer.parseInt(mPref.getString(mContext.getString(R.string.WindsAloftCeiling), "39"));
+        } catch (Exception x) {
+            return 39;
+        }
+    }
     public boolean isVerticalPfd() {
         return mPref.getBoolean(mContext.getString(R.string.VerticalPfd), false);
     }
